25a891b026a79159f5758037f2b14d955b8d8ddb
Bian Péter <bianpeti@gmail.com>
2021-03-03 11:42:38 +0100

    adding input tags and userdata, stringfy + css

--- frontend/express_react/src/App.js
+++ frontend/express_react/src/App.js
@@ -14,7 +14,19 @@ function App() {
   const handleSubmission = () => {
     const formData = new FormData();
 
+		let userData = {};
+
+		const ls = document.querySelectorAll(".toJson");
+
+		for (let i = 0; i < ls.length; i++) {
+			let name = ls[i].getAttribute("name");
+			userData[`${name}`] = ls[i].value;
+		}
+
+		console.log(userData);
+
 		formData.append("userfile", selectedFile);
+		formData.append("userData", JSON.stringify(userData));
 
 		fetch(
 			"http://localhost:8000/upload/",
@@ -33,20 +45,46 @@ function App() {
   
 
   return (
-    <div>
-			<input type="file" name="file" onChange={changeHandler} />
-			{isSelected ? (
-				<div>
-					<p>Filename: {selectedFile.name}</p>
-					<p>Filetype: {selectedFile.type}</p>
-					<p>Size in bytes: {selectedFile.size}</p>
-				</div>
-			) : (
-				<p>Select a file to show details</p>
-			)}
+    <div className="main">
+			<h1>Form</h1>
 			<div>
-				<button onClick={handleSubmission}>Submit</button>
+				<label>Name</label>
+				<div>
+					<input type="text" id="firstname" className="toJson" name="firstname" placeholder="First Name"></input>
+					<input type="text" id="lastname" className="toJson" name="lastname" placeholder="Last Name"></input>
+				</div>
 			</div>
+			<div>
+				<label>Address</label>
+				<div className="address">
+					<input type="text" id="street" className="toJson" name="street" placeholder="Street Address"></input>
+					<div className="">
+						<input type="text" id="city" className="toJson" name="city" placeholder="City"></input>
+						<input type="text" id="state" className="toJson" name="state" placeholder="State/Province"></input>
+						<input type="number" id="postalcode" className="toJson" name="postalcode" placeholder="Postal / Zip Code"></input>
+					</div>
+				</div>
+			</div>
+			<div className="email">
+				<label>E-mail</label>
+				<input type="email" className="toJson" id="email" name="email" placeholder="E-mail"></input>
+			</div>
+			<div className="fileupload-container">
+				<label>File Upload</label>
+				<div className="fileupload">
+					<input type="file" className="fileinput" name="file" onChange={changeHandler} />
+					{isSelected ? (
+						<div className="fileselect_data">
+							<p>Filename: {selectedFile.name}</p>
+							<p>Filetype: {selectedFile.type}</p>
+							<p>Size in bytes: {selectedFile.size}</p>
+						</div>
+					) : (
+						<p className="fileselect">Select a file to show details</p>
+					)}
+				</div>
+			</div>
+			<button onClick={handleSubmission}>Submit</button>
 		</div>
   );
 }
